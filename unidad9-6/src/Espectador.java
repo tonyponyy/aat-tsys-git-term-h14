b2af4b0f72b2eb3962a7c93027f7f85b651d77dc
DESKTOP-SUK4OIH\anton <antoniandreut@gmail.com>
2023-10-09 18:29:35 +0200

    added Espectador and pelicula

---
+++ unidad9-6/src/Espectador.java
@@ -0,0 +1,13 @@
+
+public class Espectador {
+	String nombre;
+	int edad;
+	double dinero;
+	
+	public Espectador(String nombre,int edad,double dinero) {
+		this.nombre = nombre;
+		this.edad = edad;
+		this.dinero = dinero;
+		
+	}
+}
---
+++ unidad9-6/src/Pelicula.java
@@ -0,0 +1,13 @@
+public class Pelicula {
+	String titulo;
+	int duracion;
+	int edad_minima;
+	String director;
+	
+	public Pelicula(String titulo,int duracion,int edad_minima, String director) {
+		this.titulo = titulo;
+		this.duracion = duracion;
+		this.edad_minima = edad_minima;
+		this.director = director;
+	}
+}
