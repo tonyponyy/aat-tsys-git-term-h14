844ba266835721d419332a68f60e6a5d0c791bd3
DESKTOP-SUK4OIH\anton <antoniandreut@gmail.com>
2023-10-12 10:24:08 +0200

    added espectators

--- unidad9-6/src/main_app.java
+++ unidad9-6/src/main_app.java
@@ -7,5 +7,16 @@ public class main_app {
 		Pelicula pelicula2 = new Pelicula("Doce hombres sin piedad", 95, 16, "Sidney Lumet");
 		Pelicula pelicula3 = new Pelicula("El señor de los anillos: Las dos torres", 179, 12, "Peter Jackson");
 		
+		
+		Espectador espectador1 = new Espectador("Jorge", 12, 9);
+		Espectador espectador2 = new Espectador("María", 15, 8);
+		Espectador espectador3 = new Espectador("Pedro", 45, 10);
+		Espectador espectador4 = new Espectador("Laura", 14, 7);
+		Espectador espectador5 = new Espectador("Carlos", 16, 6);
+		Espectador espectador6 = new Espectador("Ana", 3, 8);
+		Espectador espectador7 = new Espectador("Luis", 33, 5);
+		Espectador espectador8 = new Espectador("Isabel", 9, 9);
+		
+		
 	}
 }
