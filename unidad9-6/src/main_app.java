da39ed10c12c6ce7dcd578da8a999c9eb4a95343
Ainara <ainarabliz@gmail.com>
2023-10-12 10:19:06 +0200

    Películas

--- unidad9-6/src/main_app.java
+++ unidad9-6/src/main_app.java
@@ -4,7 +4,8 @@ public class main_app {
 		System.out.println("ejercicio 6");
 		
 		Pelicula pelicula1 = new Pelicula("Harry Potter: el prisonero de Azkaban", 137, 12, "Alfonso Cuarón");
-		Pelicula 
+		Pelicula pelicula2 = new Pelicula("Doce hombres sin piedad", 95, 16, "Sidney Lumet");
+		Pelicula pelicula3 = new Pelicula("El señor de los anillos: Las dos torres", 179, 12, "Peter Jackson");
 		
 	}
 }
